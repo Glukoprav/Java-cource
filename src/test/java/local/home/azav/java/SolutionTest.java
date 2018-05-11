5d7e6bfaf82188b68f98d9820b7907173cb562de
azavr <aazav@inbox.ru>
2018-05-11 19:54:15 +0300

    First test file

---
+++ src/test/java/local/home/azav/java/SolutionTest.java
@@ -0,0 +1,56 @@
+package local.home.azav.java;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    private Solution sol;
+
+//    @Before
+//    public void initTest() {
+//        Solution sol = new Solution();
+//    }
+
+    @Test
+    public void test1() throws Exception {
+        sol = new Solution();
+        StringBuilder sb = new StringBuilder("sdf,er,ertert,");
+        assertEquals("sdf,ertert", sol.sol2037(sb,3));
+        sol = null;
+    }
+
+    @Test
+    public void test2() throws Exception {
+        sol = new Solution();
+        StringBuilder sb = new StringBuilder(",,");
+        assertEquals("", sol.sol2037(sb,3));
+        sol = null;
+    }
+    @Test
+    public void test3() throws Exception {
+        sol = new Solution();
+        StringBuilder sb = new StringBuilder("sdf");
+        assertEquals("sdf", sol.sol2037(sb,3));
+        sol = null;
+    }
+    @Test
+    public void test4() throws Exception {
+        sol = new Solution();
+        StringBuilder sb = new StringBuilder("we");
+        assertEquals("", sol.sol2037(sb,3));
+        sol = null;
+    }
+    @Test
+    public void test5() throws Exception {
+        sol = new Solution();
+        StringBuilder sb = new StringBuilder("sdf,d");
+        assertEquals("sdf", sol.sol2037(sb,2));
+        sol = null;
+    }
+
+
+//    @After
+//    public void afterTest() {
+//        sol = null;
+//    }
+}
