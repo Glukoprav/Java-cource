2044749dc3fcec773656055e3c8afcc1c9bd8b01
azavr <aazav@inbox.ru>
2018-05-11 17:58:13 +0300

    Second

--- src/main/java/local/home/azav/java/Solution.java
+++ src/main/java/local/home/azav/java/Solution.java
@@ -1,6 +1,72 @@
 package local.home.azav.java;
 
 
+
+//Задача N 2037. Строки. Слишком короткие слова. Версия 2.
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        StringBuilder strin = new StringBuilder(scanner.next());
+        int min_word = scanner.nextInt();
+        if (min_word == 0) {
+            System.out.println(strin);
+        } else {
+            char ch;
+            int posLeft = 0, posRight = 0;
+            while (strin.charAt(posRight) == ',') {
+                posRight++;
+            }
+            strin.delete(posLeft,posRight);
+            posLeft = 0;
+            posRight = 0;
+            int length = strin.length();
+            while (posRight < length) {
+                ch = strin.charAt(posRight);
+                if (ch != ',') {
+                    posRight++;
+                } else if (ch == ',' & (posRight - posLeft) >= min_word) {
+                    posRight++;
+                    posLeft = posRight;
+                } else if (ch == ',' & (posRight == posLeft)) {
+                    strin.delete(posLeft,posRight+1);
+                    length = strin.length();
+                } else if (ch == ',' & (posRight - posLeft) < min_word) {
+                    strin.delete(posLeft,posRight+1);
+                    length = strin.length();
+                    posRight = posLeft;
+                }
+            }
+            length = strin.length();
+            if (length > 0 && strin.charAt(length-1) == ',') {
+                strin.delete(length-1,length);
+            }
+            System.out.println(strin);
+        }
+    }
+}
+
+//Задача N 2037. Строки. Слишком короткие слова
+//Дан текст, состоящий только из маленьких латинских букв и запятых.
+// Последовательность подряд идущих букв, окруженную запятыми (началом, концом строки),
+// назовем словом. Слово может быть пустым.
+//    Ваша задача — удалить из текста все слова короче k символов.
+//        Входные данные
+// Первая строка входных данных — это текст, состоящий только из строчных
+// латинских букв и запятых. Его длина не превосходит 1000 символов.
+// На второй строке находится единственное число k — минимальная допустимая длина
+// слова (0 ≤ k ≤ 1000).
+//        Выходные данные
+// Выведите текст в таком же формате, как он задан — слова следует разделять запятыми.
+//     input.txt                       output.txt
+//    ,a,ab,abc,abcd                  ab,abc,abcd
+//    2
+//,pam,,param,,param,,pam,,pam,        param,param
+//4
+
+
+
 //Задача N 2025. Массивы. Запросы минимума на подмассиве.  Версия итоговая!
 //import java.util.*;
 //public class Solution {
@@ -140,68 +206,7 @@ package local.home.azav.java;
 //    }
 //}
 
-//Задача N 2037. Строки. Слишком короткие слова. Версия 2.
-import java.util.Scanner;
-public class Solution {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        StringBuilder strin = new StringBuilder(scanner.next());
-        int min_word = scanner.nextInt();
-        if (min_word == 0) {
-            System.out.println(strin);
-        } else {
-            char ch;
-            int posLeft = 0, posRight = 0;
-            while (strin.charAt(posRight) == ',') {
-                posRight++;
-            }
-            strin.delete(posLeft,posRight);
-            posLeft = 0;
-            posRight = 0;
-            int length = strin.length();
-            while (posRight < length) {
-                ch = strin.charAt(posRight);
-                if (ch != ',') {
-                    posRight++;
-                } else if (ch == ',' & (posRight - posLeft) >= min_word) {
-                    posRight++;
-                    posLeft = posRight;
-                } else if (ch == ',' & (posRight == posLeft)) {
-                    strin.delete(posLeft,posRight+1);
-                    length = strin.length();
-                } else if (ch == ',' & (posRight - posLeft) < min_word) {
-                    strin.delete(posLeft,posRight+1);
-                    length = strin.length();
-                    posRight = posLeft;
-                }
-            }
-            length = strin.length();
-            if (length > 0 && strin.charAt(length-1) == ',') {
-                strin.delete(length-1,length);
-            }
-            System.out.println(strin);
-        }
-    }
-}
-
-//Задача N 2037. Строки. Слишком короткие слова
-//Дан текст, состоящий только из маленьких латинских букв и запятых.
-// Последовательность подряд идущих букв, окруженную запятыми (началом, концом строки),
-// назовем словом. Слово может быть пустым.
-//    Ваша задача — удалить из текста все слова короче k символов.
-//        Входные данные
-// Первая строка входных данных — это текст, состоящий только из строчных
-// латинских букв и запятых. Его длина не превосходит 1000 символов.
-// На второй строке находится единственное число k — минимальная допустимая длина
-// слова (0 ≤ k ≤ 1000).
-//        Выходные данные
-// Выведите текст в таком же формате, как он задан — слова следует разделять запятыми.
-//     input.txt                       output.txt
-//    ,a,ab,abc,abcd                  ab,abc,abcd
-//    2
-//,pam,,param,,param,,pam,,pam,        param,param
-//4
 
 
 
