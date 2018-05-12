bbdcd037d0adaa53e6d4e157cbb44e6ce40a0b72
Azav <aazav@inbox.ru>
2018-05-12 14:07:20 +0300

    рабочая версия 1

--- src/main/java/local/home/azav/java/Person.java
+++ src/main/java/local/home/azav/java/Person.java
@@ -10,15 +10,36 @@ public class Person {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getNameSpouse() {
+        return (spouse != null) ? spouse.name : "no";
+    }
+
     /**
      * This method checks gender of persons. If genders are not equal - tries to marry.
-     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     * If one of them has another spouse - execute divorce
+     * (sets spouse = null for husband and wife.
+     * Example: if both persons have spouses - then divorce will set 4 spouse to null)
+     * and then executes marry().
      *
      * @param person - new husband/wife for this person.
-     * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
+     * @return - returns true if this person has another gender than passed person
+     * and they are not husband and wife, false otherwise
      */
     public boolean marry(Person person) {
-        return ...
+        if (this.man != person.man && this.spouse != person) {
+            if (this.spouse != null) {
+                this.divorce();
+            }
+            this.spouse = person;
+            person.marry(this);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -27,7 +48,13 @@ public class Person {
      * @return true - if person status has been changed
      */
     public boolean divorce() {
-        return ???
+        if (spouse != null) {
+            spouse.spouse = null;
+            this.spouse = null;
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
