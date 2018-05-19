bc54831e5dd305bc525deefc9ac87ca403931d62
Azav <aazav@inbox.ru>
2018-05-19 17:07:43 +0300

    начальная версия

---
+++ src/main/java/local/home/azav/java/countMap/CountMapIml.java
@@ -0,0 +1,40 @@
+package local.home.azav.java.countMap;
+
+import java.util.Map;
+
+public class CountMapIml implements CountMap{
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public int getCount(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int remove(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void addAll(CountMap source) {
+
+    }
+
+    @Override
+    public Map toMap() {
+        return null;
+    }
+
+    @Override
+    public void toMap(Map destination) {
+
+    }
+}
