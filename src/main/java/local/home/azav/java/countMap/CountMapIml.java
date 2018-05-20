78d28863b4998d4c6347940d38fd70481bf6ab87
Azav <aazav@inbox.ru>
2018-05-20 23:37:39 +0300

    рабочая версия 1

--- src/main/java/local/home/azav/java/countMap/CountMapIml.java
+++ src/main/java/local/home/azav/java/countMap/CountMapIml.java
@@ -1,40 +1,88 @@
 package local.home.azav.java.countMap;
 
-import java.util.Map;
+import java.util.*;
 
-public class CountMapIml implements CountMap{
-    @Override
-    public void add(Object o) {
+public class CountMapIml<T> implements CountMap<T> {
+    private List<T> list;
 
+    /**
+     * Конструктор списка
+     */
+    public CountMapIml() {
+        list = new ArrayList<>();
     }
 
+    /**
+     * Добавляет элемент в этот контейнер.
+     */
     @Override
-    public int getCount(Object o) {
-        return 0;
+    public void add(T t) {
+        list.add(t);
     }
 
+    /**
+     * Возвращает количество добавлений данного элемента
+     */
     @Override
-    public int remove(Object o) {
-        return 0;
+    public int getCount(T t) {
+        int count = 0;
+        for (T ter : list) {
+            if (ter.equals(t)) {
+                count++;
+            }
+        }
+        return count;
     }
 
+    /**
+     * Удаляет элемент из контейнера и возвращает количество его добавлений(до удаления)
+     */
+    @Override
+    public int remove(T t) {
+        int count = getCount(t);
+        list.remove(list.indexOf(t));
+        return count;
+    }
+
+    /**
+     * Количество разных элементов
+     */
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
+    /**
+     * Добавить все элементы из source в текущий контейнер, при совпадении ключей, суммировать значения
+     */
     @Override
-    public void addAll(CountMap source) {
-
+    public void addAll(CountMap<? extends T> source) {
+        list.addAll((Collection<? extends T>) source);
     }
 
+    /**
+     * Вернуть java.util.Map. Ключ - добавленный элемент, значение - количество его добавлений
+     */
     @Override
     public Map toMap() {
-        return null;
+        Map<T, Integer> hash = new HashMap<>();
+        for (T ter : list) {
+            if (!hash.containsKey(ter)) {
+                hash.put(ter, getCount(ter));
+            }
+        }
+        return hash;
     }
 
+    /**
+     * Тот же самый контракт как и toMap(), только всю информацию записать в destination
+     */
     @Override
     public void toMap(Map destination) {
-
+        for (T ter : list) {
+            if (!destination.containsKey(ter)) {
+                destination.put(ter, getCount(ter));
+            }
+        }
     }
 }
