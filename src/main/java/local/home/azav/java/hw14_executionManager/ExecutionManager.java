b7c056b1a290285e97368953baf9fb4d8b2b8dd1
Azav <aazav@inbox.ru>
2018-06-24 13:54:30 +0300

    интерфейсы по 14 лекции

---
+++ src/main/java/local/home/azav/java/hw14_executionManager/ExecutionManager.java
@@ -0,0 +1,11 @@
+package local.home.azav.java.hw14_executionManager;
+
+public interface ExecutionManager {
+    /**
+     * Метод execute принимает массив тасков, это задания которые ExecutionManager
+     * должен выполнять параллельно (в вашей реализации пусть будет в своем пуле потоков).
+     * После завершения всех тасков должен выполниться callback (ровно 1 раз).
+     * Метод execute – это неблокирующий метод, который сразу возвращает объект Context.
+     */
+    Context execute(Runnable callback, Runnable... tasks);
+}
