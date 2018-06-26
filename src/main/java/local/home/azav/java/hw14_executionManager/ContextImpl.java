49d8c1af2f7ac19110518d09f2eeddd2c3a99a1a
Azav <aazav@inbox.ru>
2018-06-26 23:35:01 +0300

    добавил ContextImpl

---
+++ src/main/java/local/home/azav/java/hw14_executionManager/ContextImpl.java
@@ -0,0 +1,28 @@
+package local.home.azav.java.hw14_executionManager;
+
+public class ContextImpl implements Context {
+    @Override
+    public int getCompletedTaskCount() {
+        return 0;
+    }
+
+    @Override
+    public int getFailedTaskCount() {
+        return 0;
+    }
+
+    @Override
+    public int getInterruptedTaskCount() {
+        return 0;
+    }
+
+    @Override
+    public void interrupt() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
