31b8027155fd72115dda4abbb933dca815779405
azavr <aazav@inbox.ru>
2018-07-09 15:53:39 +0300

    дополнил ContextImpl

--- src/main/java/local/home/azav/java/hw14_executionManager/ContextImpl.java
+++ src/main/java/local/home/azav/java/hw14_executionManager/ContextImpl.java
@@ -1,24 +1,34 @@
 package local.home.azav.java.hw14_executionManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContextImpl implements Context {
+    private volatile int failedNumber = 0;
+    private volatile int completedNumber = 0;
+    private volatile int interruptedNumber = 0;
+    private List<Thread> threadsList = new ArrayList<>();
+
     @Override
     public int getCompletedTaskCount() {
-        return 0;
+        return completedNumber;
     }
 
     @Override
     public int getFailedTaskCount() {
-        return 0;
+        return failedNumber;
     }
 
     @Override
     public int getInterruptedTaskCount() {
-        return 0;
+        return interruptedNumber;
     }
 
     @Override
     public void interrupt() {
-
+        threadsList
+                .stream()
+                .forEach(t -> t.interrupt());
     }
 
     @Override
