389f26e658ec56730857af6e7b069dcf10f6d154
azavr <aazav@inbox.ru>
2018-07-11 15:30:16 +0300

    добавил к SchoolChatClient поток вывода сообщений от сервера

--- src/main/java/local/home/azav/java/hw15_socket_schoolChat/SchoolChatClient.java
+++ src/main/java/local/home/azav/java/hw15_socket_schoolChat/SchoolChatClient.java
@@ -23,6 +23,8 @@ public class SchoolChatClient {
         numberClient++;
     }
 
+
+
     public static void main(String[] args) {
         try (Socket socket = new Socket(ADDRESS, PORT);
              InputStream inStream = socket.getInputStream();
@@ -30,13 +32,15 @@ public class SchoolChatClient {
             SchoolChatClient clientChat = new SchoolChatClient(socket, new DataInputStream(inStream), new DataOutputStream(outStream));
             System.out.println(String.format("Коннект с сервером, client ip %s port %s", socket.getInetAddress(), socket.getPort()));
             System.out.println("Стартовал клиент: " + SchoolChatClient.numberClient);
-            // считываем логин
+            // Поднимаем поток сообщений от сервера
+            clientChat.startMessageFromServer();
+            // Cчитываем логин
             Scanner scanner = new Scanner(System.in);
             clientChat.loginClient = clientChat.inputLogin(scanner);
-            // засылаем логин на сервер
+            // Засылаем логин на сервер
             clientChat.outputStream.writeUTF(clientChat.loginClient);
             clientChat.outputStream.flush();
-            // отправляем сообщения серверу
+            // Отправляем сообщения серверу, пока не введем "exit"
             String message = "";
             do {
                 System.out.println("[" + clientChat.loginClient + "] >");
@@ -52,6 +56,11 @@ public class SchoolChatClient {
         }
     }
 
+    private void startMessageFromServer() {
+        Thread threadMess = new Thread(new MessageFromServer());
+        threadMess.start();
+    }
+
     private String inputLogin(Scanner scanner) {
         System.out.print("Введите логин: ");
         String login = scanner.next();
@@ -61,5 +70,23 @@ public class SchoolChatClient {
             login = scanner.next();
         }
         return login;
+    }
+
+    // Поток для вывода сообщений от сервера
+    private class MessageFromServer implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        break;
+                    }
+                    System.out.println(inputStream.readUTF());
+                }
+            } catch (IOException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
     }
 }
