ea97b3d38ea879955686b94d4fb9d6b4d84c6e3c
Azav <aazav@inbox.ru>
2018-07-28 17:06:58 +0300

    задача по Spring

---
+++ src/main/java/local/home/azav/java/hw23_spring_task1/Task1_Spring.java
@@ -0,0 +1,7 @@
+package local.home.azav.java.hw23_spring_task1;
+
+/** Задача 1 освоения Spring, по Youtube-лекциям
+ * https://www.youtube.com/watch?v=3wBteulZaAs&list=PL6jg6AGdCNaWF-sUH2QDudBRXo54zuN1t&index=1 */
+public class Task1_Spring {
+
+}
