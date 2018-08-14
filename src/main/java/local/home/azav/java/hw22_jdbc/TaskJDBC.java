c6c02b1a7a36a5f907f5e366a094b6738f7ba95a
Azav <aazav@inbox.ru>
2018-08-15 00:12:30 +0300

    сделал тестовый кейс для TaskJDBC (22)

--- src/main/java/local/home/azav/java/hw22_jdbc/TaskJDBC.java
+++ src/main/java/local/home/azav/java/hw22_jdbc/TaskJDBC.java
@@ -7,12 +7,34 @@ import java.sql.*;
  * скриптом user_order.sql из пакета hw21_sql_database
  */
 public class TaskJDBC {
-    public static void main(String[] args) {
+
+    protected Object forNameH2() {
         try {
-            //Class.forName("oracle.jdbc.driver.OracleDriver");
-            Class.forName("org.h2.Driver");
+            return Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+            return null;
+        }
+    }
+
+    protected int printUserOrderItem(ResultSet resultSet) throws SQLException {
+        int countResult = 0;
+        while (resultSet.next()) {
+            System.out.println("User: " + resultSet.getString("user_name")
+                    + " Date: " + resultSet.getDate("date_order")
+                    + " Item: " + resultSet.getString("item_name")
+                    + " Value: " + resultSet.getInt("value")
+                    + " Price: " + resultSet.getBigDecimal("price")
+                    + " Order_cost: " + resultSet.getBigDecimal("order_cost"));
+            countResult++;
+        }
+        return countResult;
+    }
+
+    public static void main(String[] args) {
+        TaskJDBC taskJDBC = new TaskJDBC();
+        if (taskJDBC.forNameH2() == null) {
+            return;
         }
         // jdbc:local.home.azav.java.hw22_refactoring_dao.h2:~/testdb
         // jdbc:local.home.azav.java.hw22_refactoring_dao.h2:~/test
@@ -21,35 +43,16 @@ public class TaskJDBC {
         try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery("select * from user_order_item order by date_order");) {
-            while (resultSet.next()) {
-//                u.id user_id,
-//                u.name user_name,
-//                o.order_id order_id,
-//                o.date_order date_order,
-//                i.name item_name,
-//                o.value value,
-//                i.price price,
-//                o.value * i.price order_cost
-                System.out.println("User: " + resultSet.getString("user_name")
-                        + " Date: " + resultSet.getDate("date_order")
-                        + " Item: " + resultSet.getString("item_name")
-                        + " Value: " + resultSet.getInt("value")
-                        + " Price: " + resultSet.getBigDecimal("price")
-                        + " Order_cost: " + resultSet.getBigDecimal("order_cost"));
-            }
+            int res = taskJDBC.printUserOrderItem(resultSet);
+            System.out.println(res + " selected string.");
             System.out.println("----------------------");
             System.out.println("Petrov");
             try (PreparedStatement preparedStatement = connection.prepareStatement("select * from user_order_item where user_id = ? order by date_order");
             ) {
                 preparedStatement.setInt(1, 2);
                 try (ResultSet resultSet2 = preparedStatement.executeQuery()) {
-                    while (resultSet2.next()) {
-                        System.out.println(" Date: " + resultSet2.getDate("date_order")
-                                + " Item: " + resultSet2.getString("item_name")
-                                + " Value: " + resultSet2.getInt("value")
-                                + " Price: " + resultSet2.getBigDecimal("price")
-                                + " Order_cost: " + resultSet2.getBigDecimal("order_cost"));
-                    }
+                    res = taskJDBC.printUserOrderItem(resultSet2);
+                    System.out.println(res + " selected string.");
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
---
+++ src/test/java/local/home/azav/java/hw22_jdbc/TaskJDBCTest.java
@@ -0,0 +1,34 @@
+package local.home.azav.java.hw22_jdbc;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.*;
+
+import static org.junit.Assert.*;
+
+public class TaskJDBCTest {
+    TaskJDBC taskJDBC;
+
+    @Before
+    public void setUp() {
+        taskJDBC = new TaskJDBC();
+
+    }
+
+    @Test
+    public void testForNameH2() {
+        assertNotNull(taskJDBC.forNameH2());
+    }
+
+    @Test
+    public void testPrintUserOrderItem() {
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("select * from user_order_item order by date_order");) {
+            assertEquals(20, taskJDBC.printUserOrderItem(resultSet));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
