1542ec254205e50482dc96c96442bbcc5b10a4da
azavr <aazav@inbox.ru>
2018-08-16 16:57:02 +0300

    добавил sonarcloud4.JPG

--- src/main/java/local/home/azav/java/hw2_person/MainPerson.java
+++ src/main/java/local/home/azav/java/hw2_person/MainPerson.java
@@ -4,61 +4,65 @@ package local.home.azav.java.hw2_person;
 // Проверяем методы женитьбы (marry) и развода (divorce)
 
 public class MainPerson {
+    private static void outer(String str) {
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
         Person pm1 = new Person(true, "Vasya");
         Person pw1 = new Person(false, "Masha");
-        System.out.println("1 step");
-        System.out.println(pm1.getName() + " on " + pm1.getNameSpouse());
-        System.out.println(pw1.getName() + " on " + pw1.getNameSpouse());
+        outer("1 step");
+        outer(pm1.getName() + " on " + pm1.getNameSpouse());
+        outer(pw1.getName() + " on " + pw1.getNameSpouse());
 
-        System.out.println("2 step");
+        outer("2 step");
         if (pm1.marry(pw1)) {
-            System.out.println(pm1.getName() + " on " + pm1.getNameSpouse());
-            System.out.println(pw1.getName() + " on " + pw1.getNameSpouse());
+            outer(pm1.getName() + " on " + pm1.getNameSpouse());
+            outer(pw1.getName() + " on " + pw1.getNameSpouse());
         } else {
-            System.out.println("not marry");
-            System.out.println(pm1.getName() + " on " + pm1.getNameSpouse());
-            System.out.println(pw1.getName() + " on " + pw1.getNameSpouse());
+            outer("not marry");
+            outer(pm1.getName() + " on " + pm1.getNameSpouse());
+            outer(pw1.getName() + " on " + pw1.getNameSpouse());
         }
 
-        System.out.println("3 step");
+        outer("3 step");
         if (pm1.marry(pw1)) {
-            System.out.println(pm1.getName() + " on " + pm1.getNameSpouse());
-            System.out.println(pw1.getName() + " on " + pw1.getNameSpouse());
+            outer(pm1.getName() + " on " + pm1.getNameSpouse());
+            outer(pw1.getName() + " on " + pw1.getNameSpouse());
         } else {
-            System.out.println("no");
-            System.out.println(pm1.getName() + " on " + pm1.getNameSpouse());
-            System.out.println(pw1.getName() + " on " + pw1.getNameSpouse());
+            outer("no");
+            outer(pm1.getName() + " on " + pm1.getNameSpouse());
+            outer(pw1.getName() + " on " + pw1.getNameSpouse());
         }
 
-        System.out.println("4 step");
+        outer("4 step");
         Person pm2 = new Person(true, "Fedya");
         Person pw2 = new Person(false, "Glasha");
         System.out.println(pm2.marry(pm1));
-        System.out.println(pm2.getName() + " on " + pm2.getNameSpouse());
-        System.out.println(pw1.getName() + " on " + pw1.getNameSpouse());
-        System.out.println(pm1.getName() + " on " + pm1.getNameSpouse());
-        System.out.println(pw2.getName() + " on " + pw2.getNameSpouse());
+        outer(pm2.getName() + " on " + pm2.getNameSpouse());
+        outer(pw1.getName() + " on " + pw1.getNameSpouse());
+        outer(pm1.getName() + " on " + pm1.getNameSpouse());
+        outer(pw2.getName() + " on " + pw2.getNameSpouse());
 
-        System.out.println("5 step");
+        outer("5 step");
         System.out.println(pm2.marry(pw1));
-        System.out.println(pm2.getName() + " on " + pm2.getNameSpouse());
-        System.out.println(pw1.getName() + " on " + pw1.getNameSpouse());
-        System.out.println(pm1.getName() + " on " + pm1.getNameSpouse());
-        System.out.println(pw2.getName() + " on " + pw2.getNameSpouse());
+        outer(pm2.getName() + " on " + pm2.getNameSpouse());
+        outer(pw1.getName() + " on " + pw1.getNameSpouse());
+        outer(pm1.getName() + " on " + pm1.getNameSpouse());
+        outer(pw2.getName() + " on " + pw2.getNameSpouse());
 
-        System.out.println("6 step");
+        outer("6 step");
         System.out.println(pw2.marry(pm1));
-        System.out.println(pm2.getName() + " on " + pm2.getNameSpouse());
-        System.out.println(pw1.getName() + " on " + pw1.getNameSpouse());
-        System.out.println(pm1.getName() + " on " + pm1.getNameSpouse());
-        System.out.println(pw2.getName() + " on " + pw2.getNameSpouse());
+        outer(pm2.getName() + " on " + pm2.getNameSpouse());
+        outer(pw1.getName() + " on " + pw1.getNameSpouse());
+        outer(pm1.getName() + " on " + pm1.getNameSpouse());
+        outer(pw2.getName() + " on " + pw2.getNameSpouse());
 
-        System.out.println("7 step");
+        outer("7 step");
         System.out.println(pw2.marry(pm2));
-        System.out.println(pm2.getName() + " on " + pm2.getNameSpouse());
-        System.out.println(pw1.getName() + " on " + pw1.getNameSpouse());
-        System.out.println(pm1.getName() + " on " + pm1.getNameSpouse());
-        System.out.println(pw2.getName() + " on " + pw2.getNameSpouse());
+        outer(pm2.getName() + " on " + pm2.getNameSpouse());
+        outer(pw1.getName() + " on " + pw1.getNameSpouse());
+        outer(pm1.getName() + " on " + pm1.getNameSpouse());
+        outer(pw2.getName() + " on " + pw2.getNameSpouse());
     }
 }
